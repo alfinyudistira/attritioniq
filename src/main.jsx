ee28484d2da1aa0253c92418cf23972e7b46e48d
alfinyudistira <alvinyoalvin44@gmail.com>
2026-03-25 20:23:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// --- TRIK DEBUGGING MOBILE ---
+window.addEventListener("error", (e) => {
+  alert(`BOM! Error: ${e.message} \nLokasi: baris ${e.lineno}`);
+});
+window.addEventListener("unhandledrejection", (e) => {
+  alert(`BOM! Promise Error: ${e.reason}`);
+});
+// -----------------------------
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
